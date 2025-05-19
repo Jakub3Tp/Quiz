29aca86e285dc5e64becba0b161084cc5523ba82
student06 <jakub.gola2@teb.edu.pl>
2025-05-19 09:34:17 +0200

    19.05.2025

--- logic/quiz.py
+++ logic/quiz.py
@@ -1,3 +1,18 @@
+import os
+from random import randint, choices
+
+from logic.question import Question
+
+
 class Quiz:
-    def __init__(self, path):
-        
+    def __init__(self, path, number_of_questions):
+        self.paths = os.listdir(path)
+        self.questions = []
+
+        if number_of_questions > len(self.paths):
+            raise ValueError("Number of question greater than number of question")
+        for i in range(number_of_questions):
+            index = randint(0, len(self.paths)-1)
+            self.questions.append(Question(self.paths[index]))
+            self.paths.pop(index)
+
